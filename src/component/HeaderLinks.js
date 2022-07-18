989c139eafb4021ee4af36aef6755fc4000f7a10
brainy <devbrainy98@gmail.com>
2022-07-18 18:36:13 +0100

    modified header files

--- src/component/HeaderLinks.js
+++ src/component/HeaderLinks.js
@@ -3,29 +3,29 @@ import { MdInsertLink, MdTextSnippet, MdContacts } from "react-icons/md";
 
 const HeaderLinks = ({changeTab}) => {
   return (
-    <div className="flex items-center text-center gap-x-16 justify-between uppercase">
+    <div className="flex items-center text-center gap-x-5 first-line:md:gap-x-16 justify-between uppercase">
         <p 
-          className="cursor-pointer group text-xl transition flex items-center gap-x-2"
+          className="cursor-pointer group text-lg md:text-xl transition flex items-center gap-x-2"
         >
-            <MdInsertLink className="text-2xl text-amber-400 group-hover:text-light" />
+            <MdInsertLink className="tex-xl md:text-2xl text-amber-400 group-hover:text-light" />
             <span 
               className="url-link text-light group-hover:text-amber-400"
               onClick={changeTab}
             >url</span>
         </p>
         <p 
-          className="cursor-pointer group text-xl transition flex items-center gap-x-2"
+          className="cursor-pointer group text-lg md:text-xl transition flex items-center gap-x-2"
         >
-            <MdTextSnippet className="text-2xl text-amber-400 group-hover:text-light" />
+            <MdTextSnippet className="tex-xl md:text-2xl text-amber-400 group-hover:text-light" />
             <span 
               className="text-light text-link group-hover:text-amber-400"
               onClick={changeTab}
             >text</span>
         </p>
         <p 
-          className="cursor-pointer group text-xl transition flex items-center gap-x-2"
+          className="cursor-pointer group text-lg md:text-xl transition flex items-center gap-x-2"
         >
-            <MdContacts className="text-2xl text-amber-400 group-hover:text-light" />
+            <MdContacts className="tex-xl md:text-2xl text-amber-400 group-hover:text-light" />
             <span 
               className="text-light contact-link group-hover:text-amber-400"
               onClick={changeTab}
